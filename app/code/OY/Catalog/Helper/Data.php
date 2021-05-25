84a0896e36f8fb77aff59df28d3abb4a2eadf4f0
osvaldo <osvaldo.stable@onetree.com>
2021-05-25 02:08:12 -0300

    Info Clases

--- app/code/OY/Catalog/Helper/Data.php
+++ app/code/OY/Catalog/Helper/Data.php
@@ -15,15 +15,39 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \OY\GymBooking\Api\GymClassRepositoryInterface $gymClassRepository
     )
     {
         $this->productRepository = $productRepository;
+        $this->gymClassRepository=$gymClassRepository;
         parent::__construct($context);
     }
 
     public function getProductPlan($id) {
         $product = $this->productRepository->getById($id);
         return $product->getData('is_plan');
+    }
+
+    public function getCountClass($id){
+        $product = $this->productRepository->getById($id);
+        if($product->getData('classes_option')){
+            $list = explode(',', $product->getData('classes_option'));
+            return count($list);
+        }
+        return 0;
+    }
+
+    public function getListClassesId($id){
+        $product = $this->productRepository->getById($id);
+        if($product->getData('classes_option')){
+            $list = explode(',', $product->getData('classes_option'));
+            return $list;
+        }
+        return [];
+    }
+
+    public function getClass($id){
+        return $this->gymClassRepository->getById($id);
     }
 }
