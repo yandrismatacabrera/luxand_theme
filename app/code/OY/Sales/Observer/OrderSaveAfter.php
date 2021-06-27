9225047512d0899e09ae3a44fc925b6d43792101
osvaldo <osvaldo.stable@onetree.com>
2021-06-27 00:09:29 -0300

    logs

--- app/code/OY/Sales/Observer/OrderSaveAfter.php
+++ app/code/OY/Sales/Observer/OrderSaveAfter.php
@@ -24,7 +24,8 @@ class OrderSaveAfter implements ObserverInterface
         \OY\Registry\Api\RegistryRepositoryInterface $registryRepository,
         \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
         \Magento\Framework\App\State $state,
-        \OY\Plan\Helper\Data $helper
+        \OY\Plan\Helper\Data $helper,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->planRepository=$planRepository;
         $this->planFactory=$planFactory;
@@ -35,6 +36,7 @@ class OrderSaveAfter implements ObserverInterface
         $this->customerRepository = $customerRepository;
         $this->state = $state;
         $this->helper=$helper;
+        $this->logger=$logger;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
@@ -46,21 +48,22 @@ class OrderSaveAfter implements ObserverInterface
         if ($customerId) { // Order Created
             $customer = $this->customerRepository->getById($customerId);
 
+            $this->logger->info('Estado: '.$orderStatus);
             if ($orderStatus == Order::STATE_COMPLETE) {
                 foreach ($order->getAllVisibleItems() as $orderItem) {
                     $options = $orderItem->getProductOptions();
-
+                    $this->logger->info('Options: ',$options['info_buyRequest']);
                     if (isset($options['info_buyRequest']) && isset($options['info_buyRequest']['options'])) {
                         foreach ($options['info_buyRequest']['options'] as $opt) {
                             if (isset($opt['date'])) {
+
                                 $product = $this->productRepository->getById($orderItem->getProductId());
                                 $model = $this->planFactory->create();
 
                                 if ($product->getData('code_interval')) {
                                     try {
-
                                         $strFrom =$this->dateFilter->filter($opt['date']);
-
+                                        $this->logger->info('From: '.date("Y-m-d H:i:s", strtotime("+3 hours", strtotime($strFrom))),);
                                         $strTo = new \DateTime($strFrom);
                                         $strTo->add(new \DateInterval($product->getData('code_interval')));
 
@@ -84,7 +87,6 @@ class OrderSaveAfter implements ObserverInterface
                                         if(strtotime($init) <= strtotime($today)){
                                             $this->deleteActivePlan($customerId);
                                         }
-
                                         $this->planRepository->save($model);
 
                                         if ($this->state->getAreaCode() == self::AREA_CODE) {
